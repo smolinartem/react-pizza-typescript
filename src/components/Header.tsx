e547f58d937db7f34ff9207a07066b229f593be3
Artem <krutopognali@yandex.ru>
2024-10-10 10:17:01 +0300

    revert

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../store/store'
 /* import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
@@ -12,72 +14,41 @@ import SideBarMenu from './SideBarMenu'
 import ButtonAccount from './ButtonAccount'
 import ButtonAuth from './ButtonAuth' */
 
-import { CupSoda, Percent, Phone, Pizza, UserRound } from 'lucide-react'
-import { NavLink } from 'react-router-dom'
+import { CupSoda, LogIn, Pizza, ShoppingCart, UserRound } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
+  const { userInfo } = useSelector((state: RootState) => state.user)
   /*   const { userInfo } = useSelector((state: RootState) => state.user)
   const [menuOpen, setMenuOpen] = useState(false)
   const toggleMenu = () => setMenuOpen(!menuOpen)
   const handleClose = () => setMenuOpen(false)
   useMenuClose(menuOpen, handleClose) */
 
-  const link_class = 'flex flex-col gap-1 items-center size-14'
-  const title_class = 'block text-xs'
-  const accent_color = '#D92121'
-  const default_color = '#525252'
   return (
-    <header className='z-40 fixed bottom-0 w-full h-20 bg-white'>
+    <header className='z-40 fixed bottom-0 w-full h-16 bg-white'>
       <div className='container flex items-center justify-between h-full rounded-t-xl shadow-one bg-white'>
-        <NavLink className={link_class} to='/'>
-          {({ isActive }) => (
-            <>
-              <Pizza color={isActive ? accent_color : default_color} />
-              <span className={title_class}>Пиццы</span>
-            </>
-          )}
-        </NavLink>
+        <Link className='flex-center size-10 rounded-full border' to='/'>
+          <Pizza />
+        </Link>
 
-        <NavLink className={link_class} to='drinks'>
-          {({ isActive }) => (
-            <>
-              <CupSoda color={isActive ? accent_color : default_color} />
-              <span className={title_class}>Напитки</span>
-            </>
-          )}
-        </NavLink>
+        <Link className='flex-center size-10 rounded-full border' to='drinks'>
+          <CupSoda />
+        </Link>
 
-        <NavLink className={link_class} to='promos'>
-          {({ isActive }) => (
-            <>
-              <Percent color={isActive ? accent_color : default_color} />
-              <span className={title_class}>Акции</span>
-            </>
-          )}
-        </NavLink>
+        {userInfo ? (
+          <Link className='flex-center size-10 rounded-full border' to='/'>
+            <UserRound />
+          </Link>
+        ) : (
+          <Link className='flex-center size-10 rounded-full border' to='/'>
+            <LogIn />
+          </Link>
+        )}
 
-        <NavLink className={link_class} to='contacts'>
-          {({ isActive }) => (
-            <>
-              <Phone color={isActive ? accent_color : default_color} />
-              <span className={title_class}>Контакты</span>
-            </>
-          )}
-        </NavLink>
-
-        <NavLink className={link_class} to='profile'>
-          {({ isActive }) => (
-            <>
-              <UserRound color={isActive ? accent_color : default_color} />
-              <span className={title_class}>Профиль</span>
-            </>
-          )}
-        </NavLink>
-
-        {/* <NavLink className={link_class} to='cart'>
-          <ShoppingBasket />
-          <span className={title_class}>Корзина</span>
-        </NavLink> */}
+        <Link className='flex-center size-10 rounded-full border' to='drinks'>
+          <ShoppingCart />
+        </Link>
       </div>
     </header>
   )
